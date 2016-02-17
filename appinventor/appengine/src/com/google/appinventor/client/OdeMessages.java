399ccff724d32c771ca357300ce11620b7cc47dc
Aubrey Colter <ajcolter@gmail.com>
2016-02-17 15:34:16 -0500

    Changed Connect/Run to Test.

--- appinventor/appengine/src/com/google/appinventor/client/OdeMessages.java
+++ appinventor/appengine/src/com/google/appinventor/client/OdeMessages.java
@@ -489,7 +489,7 @@ public interface OdeMessages extends Messages {
   String deleteKeystoreMenuItem();
 
   //Connect
-  @DefaultMessage("Run")
+  @DefaultMessage("Test")
   @Description("Label of the button leading to Connect related cascade items")
   String connectTabName();
 
